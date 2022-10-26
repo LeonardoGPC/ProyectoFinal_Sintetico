2fc946c46b753f69f4c81ee614bb0af6b24c0710
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-10-26 18:02:57 -0300

    detalle front, falta poner bonito y se rompio la fuente :c

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,71 @@
-
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Landing from "./components/Landing/landing.jsx"
-import Navbar from './components/NavBar/Navbar';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar/Navbar";
+import Detail from "./components/Detail/detail";
 
 function App() {
   return (
     <div className="App">
-      <div className='App_bg'>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Landing/>}/>
-        <Route path='/clubs' element={<div><h1>Clubs</h1></div>}/>
-        <Route path='/login' element={<div><h1>Login</h1></div>}/>
-        <Route path='/sintetico' element={<div><h1>Home</h1></div>}/>
-        <Route path='/profile' element={<div><h1>Profile</h1></div>}/>
-        <Route path='/sintetico/detail/:id' element={<div><h1>Detail</h1></div>}/>
-        <Route path='/create' element={<div><h1>Create</h1></div>}/>
-        <Route path='*' element={<div><h1>Error 404</h1></div>}/>
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Landing</h1>
+            </div>
+          }
+        />
+        <Route
+          path="/clubs"
+          element={
+            <div>
+              <h1>Clubs</h1>
+            </div>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <div>
+              <h1>Login</h1>
+            </div>
+          }
+        />
+        <Route
+          path="/sintetico"
+          element={
+            <div>
+              <h1>Home</h1>
+            </div>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <div>
+              <h1>Profile</h1>
+            </div>
+          }
+        />
+        <Route path="/sintetico/detail/:id" element={<Detail />} />
+        <Route
+          path="/create"
+          element={
+            <div>
+              <h1>Create</h1>
+            </div>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <div>
+              <h1>Error 404</h1>
+            </div>
+          }
+        />
       </Routes>
-      </div>
     </div>
   );
 }
