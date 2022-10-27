31153df36ed84efe64684a36efa08a170a7efc4b
paosu95 <90468038+paosu95@users.noreply.github.com>
2022-10-27 13:04:34 -0500

    Diseño de las cards casi listo

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,21 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Landing from "./components/Landing/landing.jsx"
-import Navbar from './components/NavBar/Navbar';
+import Home from './components/Home/Home.jsx'
+
+
 
 
 function App() {
   return (
     <div className="App">
       <div className='App_bg'>
+        
       <Routes>
         <Route path='/' element={<Landing/>}/>
         <Route path='/clubs' element={<div><h1>Clubs</h1></div>}/>
         <Route path='/login' element={<div><h1>Login</h1></div>}/>
-        <Route path='/sintetico' element={<div><h1>Home</h1></div>}/>
+        <Route path='/sintetico' element={<Home/>}/>
         <Route path='/profile' element={<div><h1>Profile</h1></div>}/>
         <Route path='/sintetico/detail/:id' element={<div><h1>Detail</h1></div>}/>
         <Route path='/create' element={<div><h1>Create</h1></div>}/>
