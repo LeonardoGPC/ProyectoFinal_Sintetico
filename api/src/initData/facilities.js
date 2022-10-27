1d59876af06a54aa90097254f0d9761cd8b2998a
Nicolas Rodriguez <nrodriguez9064@misena.edu.co>
2022-10-26 19:20:49 -0500

    Carga de data

--- api/src/initData/facilities.js
+++ api/src/initData/facilities.js
@@ -1,6 +1,8 @@
-export const FACILITIES = [
-    { id: 0, name: "Duchas" },
-    { id: 1, name: "Estacionamiento" },
-    { id: 2, name: "Buffet" },
-    { id: 3, name: "Quincho" }
-]
+module.exports =  {
+    FACILITIES : [
+        { id: 0, name: "Duchas" },
+        { id: 1, name: "Estacionamiento" },
+        { id: 2, name: "Buffet" },
+        { id: 3, name: "Quincho" }
+    ]
+}
--- api/src/initData/surfaces.js
+++ api/src/initData/surfaces.js
@@ -1,6 +1,8 @@
-export const SURFACES = [
-    { id: 0, name:"sintético" },
-    { id: 1, name: "cesped" },
-    { id: 2, name: "parquet" },
-    { id: 3, name: "cemento" },
-];
+module.exports = {
+    SURFACES : [
+        { id: 0, name:"sintético" },
+        { id: 1, name: "cesped" },
+        { id: 2, name: "parquet" },
+        { id: 3, name: "cemento" },
+    ]
+}
