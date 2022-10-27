a7212e890f59e972b88bac6169b01a48e499c433
Nicolas Rodriguez <nrodriguez9064@misena.edu.co>
2022-10-27 12:39:01 -0500

    refactorizacion queryparams y createfield

--- api/src/controllers/fieldController.js
+++ api/src/controllers/fieldController.js
@@ -1,53 +1,74 @@
 const axios = require('axios');
 const { Field, Facility, Surface, Size } = require('../db');
+const { Op } = require("sequelize");
+
+const queryParams = {
+    attributes: ['name'],
+    through: {
+        attributes: [],
+    },
+};
 
 
-
+ 
 async function getFields(){
-    let fields = await Field.findAll();
+    let fields = await Field.findAll({
+        where: {
+            isDeleted: {
+                [Op.eq]: false
+            }
+        },
+        include:{
+            model: Size,
+            ...queryParams,
+            model: Surface,
+            ...queryParams,
+            model: Facility,
+            ...queryParams
+        },
+        order: [
+            ['id', 'ASC']
+        ],
+    });
     if(fields.length) return fields
     else throw new Error('there is no data in db') 
 }
 
 async function getFieldById(id){
     const field = await Field.findByPk(id, {
+        where: {
+            isDeleted: {
+                [Op.eq]: false
+            }
+        },
         include:{
             model: Size,
-            attributes: ['name'],
-            through: {
-                attributes: [],
-            },
+            ...queryParams,
             model: Surface,
-            attributes: ['name'],
-            through: {
-                attributes: [],
-            },
+            ...queryParams,
             model: Facility,
-            attributes: ['name'],
-            through: {
-                attributes: [],
-            }
+            ...queryParams
         }
     })
-    if(field.length) return field
+    if(field) return field
     else throw new Error('Field does not exist in db') 
 }
 
-async function createField(field){
-    var { id, name, image, state, price, address, openHour, closeHour, facilities, size, surface, city } = field;
-    var field = { id, name, image, state, price, address, openHour, closeHour };
+async function createField(fieldData){
+    const { id, name, image, state, price, address, openHour, closeHour, facilities, size, surface, city } = fieldData;
+    const field = { id, name, image, state, price, address, openHour, closeHour };
     try{
-        var newField = await Field.create(field);
-        newField = await newField.setSize(size);
-        newField = await newField.setSurface(surface);
-        newField = await newField.setCity(city)
+        const newField = await Field.create(field);
+        await newField.setSize(size);
+        await newField.setSurface(surface);
+        await newField.setCity(city)
         if (facilities){
             await newField.addFacilities(facilities);
         }
-        return newField;
+        return "Field created successfully";
     }catch(error){
         console.log(error);
-       throw new Error("Campos inválidos");
+       throw new Error("Invalid Fields");
     }
     
 }
