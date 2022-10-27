9cbe95807574ad86ccf708137ed0ce9058d27de3
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-10-26 22:43:14 -0300

    CSS basico de la ruta detalle

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Landing from "./components/Landing/landing.jsx"
 import Navbar from './components/NavBar/Navbar';
+import Detail from './components/Detail/detail';
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
         <Route path='/login' element={<div><h1>Login</h1></div>}/>
         <Route path='/sintetico' element={<div><h1>Home</h1></div>}/>
         <Route path='/profile' element={<div><h1>Profile</h1></div>}/>
-        <Route path='/sintetico/detail/:id' element={<div><h1>Detail</h1></div>}/>
+        <Route path='/sintetico/detail/:id' element={<Detail/>}/>
         <Route path='/create' element={<div><h1>Create</h1></div>}/>
         <Route path='*' element={<div><h1>Error 404</h1></div>}/>
       </Routes>
