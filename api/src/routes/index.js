94a037ba8a4cd7979a5cd35521cb6dcdbc256dd7
MaxiJ <107537632+maxijayme@users.noreply.github.com>
2022-10-27 17:00:56 +0200

    actualizado hasta el 26-10

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,11 @@
 const { Router } = require('express');
 const fields = require('./fields')
 const facilities = require('./facilities')
-const facilities = require('./sizes')
+const facilities = require('./facilities')
 const router = Router();
 
 router.use('/fields', fields)
 router.use('/sizes', sizes)
 router.use('/facilities', facilities)
-router.use('/surfaces', surfaces)
 
 module.exports = router;
