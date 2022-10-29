56d988f5c9bddafe394ff817f22d47f2dcb7cf5e
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-10-29 05:25:44 -0300

    actualizacion pagina detail

--- client/src/components/Detail/detail.jsx
+++ client/src/components/Detail/detail.jsx
@@ -2,9 +2,21 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../NavBar/Navbar";
 import styles from "./detail.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getFieldDetail } from "../../redux/actions";
 import MiniFooter from "../MiniFooter/MiniFooter";
+import map from "../../img/icons/map.png";
+import buffet from "../../img/icons/buffet.png";
+import duchas from "../../img/icons/duchas.png";
+import estacionamiento from "../../img/icons/estacionamiento.png";
+import quincho from "../../img/icons/quincho.png";
+import { Rate } from "antd";
+import { FaStar } from "react-icons/fa";
+
+const colors = {
+  orange: "#FFBA5A",
+  grey: "#a9a9a9",
+};
 
 function Detail() {
   const { id } = useParams();
@@ -12,9 +24,26 @@ function Detail() {
   const detailField = useSelector((state) => state.detail);
   console.log(detailField);
 
+  const [currentValue, setCurrentValue] = useState(0);
+  const [hoverValue, setHoverValue] = useState(undefined);
+  const stars = Array(5).fill(0);
+
+  const handleClick = (value) => {
+    setCurrentValue(value);
+    alert(`Hiciste una puntuación de ${value} estrellas`)
+  };
+
+  const handleMouseOver = (newHoverValue) => {
+    setHoverValue(newHoverValue);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverValue(undefined);
+  };
   useEffect(() => {
     dispatch(getFieldDetail(id));
   }, [dispatch]);
+
   return (
     <div>
       <Navbar />
@@ -24,10 +53,7 @@ function Detail() {
             <div className={styles.cardContainer}>
               <figure className={styles.image}>
                 {/* <button>←</button> */}
-                <img
-                  src={detailField.image}
-                  alt="cancha"
-                />
+                <img src={detailField.image} alt="cancha" />
                 {/* <button>→</button> */}
               </figure>
               <div className={styles.info}>
@@ -38,19 +64,50 @@ function Detail() {
                 <div className={styles.content}>
                   <ul>
                     <li>Rating: {detailField.score}</li>
-                    <li>Cancha para {detailField.Size.name} jugadores</li>
-                    <li>Tipo de suelo: {detailField.Surface.name}</li>
-                    <li>Ubicación: {detailField.City.name}</li>
+                    {/* <li><Rate disabled allowHalf value={detailField.score} character="★" style={{ fontSize: 30}}/></li> */}
+                    <li className={styles.facilities}>
+                      {detailField.Facilities?.map((el) => {
+                        return (
+                          <p
+                            className={styles.facilitieIcon}
+                            style={{ margin: 3 }}
+                          >
+                            {el.name === "Duchas" ? (
+                              <img
+                                src={duchas}
+                                style={{ height: 25 }}
+                                alt={el.name}
+                              />
+                            ) : el.name === "Estacionamiento" ? (
+                              <img
+                                src={estacionamiento}
+                                style={{ height: 25 }}
+                              />
+                            ) : el.name === "Buffet" ? (
+                              <img src={buffet} style={{ height: 25 }} />
+                            ) : (
+                              <img src={quincho} style={{ height: 25 }} />
+                            )}
+                          </p>
+                        );
+                      })}
+                    </li>
+
+                    <li>
+                      Cancha para {detailField.Size ? detailField.Size.name : 0}{" "}
+                      jugadores
+                    </li>
+                    {/* <li>Tipo de suelo: {detailField.Surface ? detailField.Surface.name : "Sin información"}</li> */}
+                    <li>
+                      <img src={map} style={{ height: 25 }} />{" "}
+                      {detailField.address},{" "}
+                      {detailField.City ? detailField.City.name : ""}
+                    </li>
                   </ul>
                   <div className={styles.description}>
-                    <p>
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                      Culpa voluptate a aut veritatis similique, voluptatum,
-                      vero quis quae ut nobis at ducimus ipsa ea id sint
-                      repellat voluptates reprehenderit perspiciatis.
-                    </p>
+                    <p>{detailField.description}</p>
                     <span className={styles.price}>
-                      <p>US${detailField.price}</p>
+                      <p>${detailField.price}</p>
                       <button>Reservar</button>
                     </span>
                   </div>
@@ -80,14 +137,45 @@ function Detail() {
                   aliquid, eveniet illo totam aliquam ab est rerum
                   necessitatibus voluptas accusamus deserunt architecto.
                 </p>
+
               </div>
             </div>
+            <div className={styles.yourComment}>
+                  <h3> Deja tu comentario </h3>
+                  <div>
+                    {stars.map((_, index) => {
+                      return (
+                        <FaStar
+                          key={index}
+                          size={24}
+                          onClick={() => handleClick(index + 1)}
+                          onMouseOver={() => handleMouseOver(index + 1)}
+                          onMouseLeave={handleMouseLeave}
+                          color={
+                            (hoverValue || currentValue) > index
+                              ? colors.orange
+                              : colors.grey
+                          }
+                          style={{
+                            marginRight: 10,
+                            cursor: "pointer",
+                          }}
+                        />
+                      );
+                    })}
+                  </div>
+                  <textarea
+                    placeholder="Deja tu comentario..."
+                    className={styles.textarea}
+                  />
+                  <button>Submit</button>
+                </div>
           </div>
         </div>
       ) : (
         <p>Cancha inexistente</p>
       )}
-      <MiniFooter/>
+      <MiniFooter />
     </div>
   );
 }
