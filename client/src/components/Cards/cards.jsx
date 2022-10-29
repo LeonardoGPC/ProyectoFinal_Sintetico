7af5f93d73520e55a085c0a11c7d72698748baee
paosu95 <90468038+paosu95@users.noreply.github.com>
2022-10-28 20:04:28 -0500

    Diseño de Cards listo y formulario de creacion listo

--- client/src/components/Cards/cards.jsx
+++ client/src/components/Cards/cards.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactStars from 'react-stars';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getFields } from '../../redux/actions';
@@ -8,14 +9,14 @@ export default function Cards() {
   const dispatch = useDispatch();
   const fields = useSelector((state) => state.fields);
 
-
-
   useEffect(() => {
     dispatch(getFields());
   }, [dispatch]);
 
   return (
     <div className={style.container}>
+      <i class="fa-thin fa-star"></i>
+
       {fields.map((field) => {
         return (
           <div key={field.id} className={style.group}>
@@ -27,45 +28,7 @@ export default function Cards() {
               />
               <div className={style.containerDescription}>
                 <h1 className={style.titulo}>{field.name}</h1>
-                <div className={style.raiting}>
-                  <p>
-                    <input
-                      id="radio1"
-                      type="radio"
-                      name="estrellas"
-                      value="5"
-                    />
-                    <label htmlFor="radio1">★</label>
-                    <input
-                      id="radio2"
-                      type="radio"
-                      name="estrellas"
-                      value="4"
-                    />
-                    <label htmlFor="radio2">★</label>
-                    <input
-                      id="radio3"
-                      type="radio"
-                      name="estrellas"
-                      value="3"
-                    />
-                    <label htmlFor="radio3">★</label>
-                    <input
-                      id="radio4"
-                      type="radio"
-                      name="estrellas"
-                      value="2"
-                    />
-                    <label htmlFor="radio4">★</label>
-                    <input
-                      id="radio5"
-                      type="radio"
-                      name="estrellas"
-                      value="1"
-                    />
-                    <label htmlFor="radio5">★</label>
-                  </p>
-                </div>
+                <ReactStars edit={false} value={field.score} count={5} size={24} color2={'#ffd700'} />
                 <p className={style.description}>
                   Disfruta de un muy buen partido de fútbol 6 con tus mejores
                   amigos o familiares, muestra todas tus habilidades en este
@@ -76,14 +39,13 @@ export default function Cards() {
               </div>
             </div>
             <div className={style.thirdContainer}>
-              <p className={style.precio}>Precio: {field.price}</p>
-              <p className={style.localidad}>Localidad: {field.CityId}</p>
-              <p className={style.tamaño}>Tamaño: futbol {field.SizeId}</p>
+              <p className={style.precio}>Precio: ${field.price}</p>
+              <p className={style.localidad}>Localidad: {field.City.name}</p>
+              <p className={style.tamaño}>Tamaño: futbol {field.Size.name}</p>
             </div>
           </div>
         );
       })}
-     
     </div>
   );
 }
