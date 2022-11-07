a385382dfab6afa8f75ce84b0598af0f0d3b85d2
GianlucaTerzoni <gianlucaterzoni@hotmail.com>
2022-11-06 23:16:42 -0300

    feat(comp/admin) basic admin dashboard

---
+++ client/src/components/Admin/GestPrecios.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Profile from '../Profile/profile'
+import style from './GestPrecios.module.css'
+
+const GestPrecios = () => {
+  return (
+    <div className={style.admin}>
+        <Profile />
+      <div className={style.container}></div>
+      <div className={style.acciones}>
+        <h3 className={style.h3}>Gestionar Precios</h3>
+        <div className={style.info}>
+          <button className={style.btn}>Actualizar Precios</button>
+          <button className={style.btn}>Modificar Precios Planes</button>
+
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default GestPrecios
---
+++ client/src/components/Admin/GestReservas.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Profile from '../Profile/profile'
+import style from './GestReservas.module.css'
+
+const GestReservas = () => {
+  return (
+    <div className={style.admin}>
+    <Profile />
+  <div className={style.container}></div>
+  <div className={style.acciones}>
+    <h3 className={style.h3}>Gestionar Reservas</h3>
+    <div className={style.info}>
+      <button className={style.btn}>Historial de Reservas</button>
+      <button className={style.btn}>Ocultar Reserva</button>
+      <button className={style.btn}>Modificar Reserva</button>
+      <button className={style.btn}>Cancelar Reserva</button>
+      </div>
+  </div>
+</div>
+  )
+}
+
+export default GestReservas
