25a6cb14d79f74ae61f2722046670bb8b3e72185
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-11-10 13:46:37 -0300

    tabla de canchas aprobadas

--- client/src/components/AdminManageFields/TableCards/TableApproved.jsx
+++ client/src/components/AdminManageFields/TableCards/TableApproved.jsx
@@ -8,47 +8,50 @@ import styles from "./TableApproved.module.css";
 export default function TableApproved() {
   const dispatch = useDispatch();
   const fields = useSelector((state) => state.fields);
-  console.log(fields);
+  // console.log(fields);
+
+  const filterField = fields.filter((f) => f.state === "APPROVED");
+  console.log(filterField);
 
   useEffect(() => {
     dispatch(getFields());
   }, []);
   return (
     <div className={styles.tablecards}>
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Cancha</th>
-            <th>Localidad</th>
-            <th>Precio</th>
-            <th>Jugadores</th>
-            <th>Ver</th>
-          </tr>
-        </thead>
-        {fields.map((el) => {
-          return el.state === "APPROVED" ? (
-              <tbody>
-                <tr key={el.id} className={styles.columns}>
-                  <th>{el.id}</th>
-                  <td>{el.name}</td>
-                  <td>{el.City.name}</td>
-                  <td>${el.price}</td>
-                  <td>{el.Size.name}</td>
-                  <td>
-                    <Link to={`/sintetico/detail/${el.id}`}>
-                      <button className={styles.detail}>
-                        <RiEdit2Line />
-                      </button>
-                    </Link>
-                  </td>
-                </tr>
-              </tbody>
-          ) : (
-            <h3></h3>
-          );
-        })}
-      </table>
+      {filterField.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Cancha</th>
+              <th>Localidad</th>
+              <th>Precio</th>
+              <th>Jugadores</th>
+              <th>Ver</th>
+            </tr>
+          </thead>
+          {filterField.map((el) => (
+            <tbody className={styles.tbody}>
+              <tr key={el.id}>
+                <th>{el.id}</th>
+                <td>{el.name}</td>
+                <td>{el.City.name}</td>
+                <td>${el.price}</td>
+                <td>{el.Size.name}</td>
+                <td>
+                  <Link to={`/sintetico/detail/${el.id}`}>
+                    <button className={styles.detail}>
+                      <RiEdit2Line />
+                    </button>
+                  </Link>
+                </td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      ) : (
+        <h3 className={styles.notfound}>No hay canchas aprobadas</h3>
+      )}
     </div>
   );
 }
