179d2f5431b3012645639e61d7ce478f33c6c8fd
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-11-10 13:50:04 -0300

    muestro en cards solo las canchas que tengan state APPROVED

--- client/src/components/Cards/cards.jsx
+++ client/src/components/Cards/cards.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactStars from 'react-stars';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { getFields } from '../../redux/actions';
-import style from './cards.module.css';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import { Link } from 'react-router-dom';
+import React from "react";
+import ReactStars from "react-stars";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { getFields } from "../../redux/actions";
+import style from "./cards.module.css";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Link } from "react-router-dom";
 
 export default function Cards() {
   const dispatch = useDispatch();
-  const fields = useSelector((state) => state.fields);
+  const fieldState = useSelector((state) => state.fields);
+  const fields = fieldState.filter((f) => f.state === "APPROVED");
+  console.log(fields);
   const [page, setPage] = useState(1);
- 
+
   const itemsPerPage = 3;
   const fieldsByPage = fields.slice(0, page * itemsPerPage);
 
@@ -33,34 +35,41 @@ export default function Cards() {
         {fieldsByPage.map((field) => {
           return (
             <div key={field.id}>
-            <Link  className={style.group} to={`/sintetico/detail/${field.id}`}>
-              <div className={style.secondContainer}>
-                <img
-                  className={style.image}
-                  src={field.image}
-                  alt="imagenCancha"
-                />
-                <div className={style.containerDescription}>
-                  <h1 className={style.titulo}>{field.name}</h1>
-                  <ReactStars
-                    edit={false}
-                    value={field.score}
-                    count={5}
-                    size={24}
-                    color2={'#98f576'}
+              <Link
+                className={style.group}
+                to={`/sintetico/detail/${field.id}`}
+              >
+                <div className={style.secondContainer}>
+                  <img
+                    className={style.image}
+                    src={field.image}
+                    alt="imagenCancha"
                   />
-                  <p className={style.description}>
-                    {field.description}
+                  <div className={style.containerDescription}>
+                    <h1 className={style.titulo}>{field.name}</h1>
+                    <ReactStars
+                      edit={false}
+                      value={field.score}
+                      count={5}
+                      size={24}
+                      color2={"#98f576"}
+                    />
+                    <p className={style.description}>{field.description}</p>
+                  </div>
+                </div>
+                <div className={style.thirdContainer}>
+                  <p className={style.precio}>Precio: ${field.price}</p>
+                  <p className={style.localidad}>
+                    Localidad: {field.City.name}
+                  </p>
+                  <p className={style.tamaño}>
+                    Tamaño: futbol {field.Size.name}
+                  </p>
+                  <p className={style.superficie}>
+                    Superficie: {field.Surface.name}
                   </p>
                 </div>
-              </div>
-              <div className={style.thirdContainer}>
-                <p className={style.precio}>Precio: ${field.price}</p>
-                <p className={style.localidad}>Localidad: {field.City.name}</p>
-                <p className={style.tamaño}>Tamaño: futbol {field.Size.name}</p>
-                <p className={style.superficie}>Superficie: {field.Surface.name}</p>
-              </div>
-            </Link>
+              </Link>
             </div>
           );
         })}
