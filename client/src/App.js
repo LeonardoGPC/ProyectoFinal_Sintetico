59ebc904fd9f1cd342686bf4cde387eefe16de49
Leonardo Peña <leonardo.html.css@gmail.com>
2022-11-12 23:13:11 -0600

    Creación de Configuración en el perfil

--- client/src/App.js
+++ client/src/App.js
@@ -61,6 +61,10 @@ function App() {
         setLoad(true);
         setTimeout(animation, 1750);
         break;
+      case '/about':
+        setLoad(true);
+        setTimeout(animation, 1750);
+        break;
     }
   }, [location]);
 
@@ -72,7 +76,7 @@ function App() {
           <Route path="/clubs" element={<Clubs />} />
           <Route path="/login" element={<Login />} />
           <Route path="/sintetico" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<Profile />}/>
           <Route path="/sintetico/detail/:id" element={<Detail />} />
           <Route path="/create" element={<Create />} />
           <Route path="*" element={<Error/>} />
