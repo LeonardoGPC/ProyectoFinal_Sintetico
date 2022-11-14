039c8d44423a39145e4f402aa9de068e200a1b49
paosu95 <90468038+paosu95@users.noreply.github.com>
2022-11-13 23:33:20 -0500

    *Realice un cambio en fieldController para poder acceder a mas info del usuario *Realice las tablas de publicaciones de cancha, reservas de las canchas y gestion de plan

--- client/src/components/Clubs/BookingsClub.jsx
+++ client/src/components/Clubs/BookingsClub.jsx
@@ -1,70 +1,69 @@
-import React from "react";
-import Cookies from "universal-cookie";
+import React from 'react';
+import Cookies from 'universal-cookie';
 import { useDispatch, useSelector } from 'react-redux';
-import { getBookings, getFields } from "../../redux/actions";
+import { getBookings, getFields } from '../../redux/actions';
 import { useEffect, useState } from 'react';
-import axios from "axios";
+import axios from 'axios';
 
-import styles from './bookingsClub.module.css'
-
-
+import styles from './bookingsClub.module.css';
 
 export default function BookingsClub() {
-   const cookie = new Cookies();
-   const [user, setUser] = useState();
-  const id = Number(cookie.get("id"));
+  const cookie = new Cookies();
+  const [user, setUser] = useState();
+  const id = Number(cookie.get('id'));
   const dispatch = useDispatch();
-  const fields = useSelector((state) => state.fields).filter((f) => f.OwnerId === id);
+  const fields = useSelector((state) => state.fields).filter(
+    (f) => f.OwnerId === id,
+  );
 
-  useEffect(() => { 
+  useEffect(() => {
     const idUser = cookie.get('id');
     const getUserData = async () => {
-    const { data } = await axios.get('http://localhost:3001/users/' + idUser);
+      const { data } = await axios.get('http://localhost:3001/users/' + idUser);
       setUser(data);
-    }
-    dispatch(
-      getBookings());
-      dispatch(
-        getFields()
-      );
-      getUserData();
+    };
+    dispatch(getBookings());
+    dispatch(getFields());
+    getUserData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className={styles.tableBookings}>
-        <div className={styles.tittle}> Reservas </div>
+      <div className={styles.tittle}> Reservas </div>
       {/* {bookings.length > 0 ? ( */}
-        
-        <table>
-          <thead>
-            <tr>
-              
-              <th>Cancha</th>
-              <th>Fecha</th>
-              <th>Horario</th>
-              <th>Cliente</th>
-              <th>Telefono</th>
-            </tr>
-          </thead>
-          <tbody className={styles.tbody}>
-           {fields.map((field) => ( 
-             field.Bookings.map((booking)=> (
+
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Cancha</th>
+            <th>Fecha</th>
+            <th>Horario</th>
+            <th>Cliente</th>
+            <th>Telefono</th>
+            <th>Correo</th>
+          </tr>
+        </thead>
+        <tbody className={styles.tbody}>
+          {fields.map((field) =>
+            field.Bookings.map((booking) => (
               <tr key={booking.id}>
+                <td>{field.id}</td>
                 <td>{field.name}</td>
                 <td>{booking.date}</td>
-                <td>{booking.hour}</td> 
+                <td>{booking.hour}</td>
                 <td>{booking.User.name}</td>
                 <td>{booking.User.phone}</td>
+                <td>{booking.User.email}</td>
               </tr>
-             ))
-           ))}
-           </tbody>
-        </table>
+            )),
+          )}
+        </tbody>
+      </table>
       {/* ) : (
         <h3 className={styles.notfound}>No hay canchas reservadas :C</h3>
       )} */}
     </div>
-
-  )
+  );
 }
