74ce036ff510dae06960ce89cca2e3b0b24a68a3
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-11-15 12:47:02 -0300

    cantidad de usuarios segun plan contratado

--- client/src/components/AdminManagePlan/TablePlan/StatsPlan.jsx
+++ client/src/components/AdminManagePlan/TablePlan/StatsPlan.jsx
@@ -1,6 +1,28 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUsers } from "../../../redux/actions";
+import styles from "./StatsPlan.module.css"
 
 export default function StatsPlan() {
+    const dispatch = useDispatch();
+    const users = useSelector((state) => state.allUsers);
+
+    const usersNone = users.filter(user => user.planType === "none").length
+    const usersBasico = users.filter(user => user.planType === "basico").length
+    const usersClub = users.filter(user => user.planType === "club").length
+    const usersPremium = users.filter(user => user.planType === "premium").length
+    const usersTotal = users.length 
+    console.log("Total de usuarios: " + usersTotal);
+
+    useEffect(() => {
+        dispatch(getUsers());
+      }, []);
   return (
-    <div>StatsPlan</div>
+    <div className={styles.statsplan}>
+        <div className={styles.none}>Gratis: {usersNone}</div>
+        <div className={styles.basico}>Básico: {usersBasico}</div>
+        <div className={styles.club}>Club: {usersClub}</div>
+        <div className={styles.premium}>Premium: {usersPremium}</div>
+    </div>
   )
 }
